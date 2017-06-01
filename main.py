5ac43dff406cf94ff31a3ff499ccbc41c74e1d67
aswinmohanme <aswinmmohan@gmail.com>
2017-06-01 19:40:32 +0530

    AMazon Booker Helper

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from amazon.api import AmazonAPI
+
+from config import AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE
+
+class AmazonBooker:
+    def __init__(self):
+        self.amazon = AmazonAPI(AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE)
+
+    def get_book_by_name(self, name):
+        return self.amazon.search_n(1, Keywords=name, SearchIndex='Books')[0]
+
+    def get_books_by_name(self, name, num):
+        return self.amazon.search_n(num, Keywords=name, SearchIndex='Books')
+
 
 if __name__ == "__main__":
+    amazonBooker = AmazonBooker()
