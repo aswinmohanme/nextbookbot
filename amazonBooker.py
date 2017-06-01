c7eb85b6a05d6376598a05f7cf38e7b0aa6a07f7
aswinmohanme <aswinmmohan@gmail.com>
2017-06-01 19:41:42 +0530

    Refactor to seprate file

---
+++ amazonBooker.py
@@ -0,0 +1,13 @@
+from amazon.api import AmazonAPI
+
+from config import AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE
+
+class AmazonBooker:
+    def __init__(self):
+        self.amazon = AmazonAPI(AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE)
+
+    def get_book_by_name(self, name):
+        return self.amazon.search_n(1, Keywords=name, SearchIndex='Books')[0]
+
+    def get_books_by_name(self, name, num):
+        return self.amazon.search_n(num, Keywords=name, SearchIndex='Books')
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from amazon.api import AmazonAPI
 
-from config import AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE
-
-class AmazonBooker:
-    def __init__(self):
-        self.amazon = AmazonAPI(AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE)
-
-    def get_book_by_name(self, name):
-        return self.amazon.search_n(1, Keywords=name, SearchIndex='Books')[0]
-
-    def get_books_by_name(self, name, num):
-        return self.amazon.search_n(num, Keywords=name, SearchIndex='Books')
-
+from amazonBooker import AmazonBooker
 
 if __name__ == "__main__":
     amazonBooker = AmazonBooker()
