88895708224667ec56929a215c68df008d0b9738
aswinmohanme <aswinmmohan@gmail.com>
2017-06-03 18:46:31 +0530

    Modify to React to Tweets Containing @bookbot tag

--- amazonBooker.py
+++ amazonBooker.py
@@ -8,11 +8,17 @@ class AmazonBooker:
         self.amazon = AmazonAPI(AWSACCESSKEY, AWSSECRETKEY, AMAZONSTORE)
 
     def get_book_by_name(self, name):
-        return random.choice(self.amazon.search_n(3, Keywords=name, SearchIndex='Books'))
+        return self.amazon.search_n(1, Keywords=name, SearchIndex='Books')[0]
 
     def get_books_by_name(self, name, num):
         return self.amazon.search_n(num, Keywords=name, SearchIndex='Books')
 
     def get_book_by_keywords(self, kw):
         return self.amazon.search_n(1, Power=kw, SearchIndex='Books')[0]
+    
+    def get_similar_books(self, name, num):
+        # book = self.amazon.search_n(1, Power='title:({})'.format(name), SearchIndex='Books')[0]
+        book = self.amazon.search_n(1, Keywords=name, SearchIndex='Books')[0]
+        books = self.amazon.similarity_lookup(ItemId=book.asin)
+        return books[:num]
     
--- main.py
+++ main.py
@@ -12,32 +12,23 @@ if __name__ == "__main__":
     amazonBooker = AmazonBooker()
     tweeter = Tweeter()
     Rake = RAKE.Rake('smartStop.txt')
-    users_tweeted = ["abookme"]
-    # tweet_body = ["Hey there @{} I just found this book {} that you would love to read #books #read",
-    #               "Hey @{} since you like reading so much, Check out {} #toread #read #books",
-    #               "@{} I just read your last tweet and thought you would love {} #toread #books",
-    #               "Hey @{} Thought you would #love {} #books #fead",
-    #               "Hey @{} Check out this awesome book that you will love {} #books",
-    #               "Hey @{} I am thinking you would #love {} #books"]
-    tweet_body = ["Hey @{} I just found {} on {} that you would love #books ",
-                  "Hey @{} since you like reading, Check {} on {} #books",
-                  "@{} I think you will love {} on {} #toread #books",
-                  "Hey @{} I bet you will fall in #love with {} on {} #books ",
-                  "Hey @{} Check out this awesome book  {} on {} #books",
-                  "Hey @{} I am thinking you would #love {} on {} #books"]
+    tweet_body = ["Hey @{} try {} on {} #books #suggest",
+                 "Hey @{} Checkout {} on {} #books #suggest",
+                 "Hey @{} This seems #interesting {} on {} #books #suggest #books",
+                 "Hey @{} just found this {} on {} for #you #suggest #books",
+                 "Hey @{} thinking about giving {} at {} a #try #suggest #books"
+                 ]
 
     # Main Function CallBack
     def process_status(status):
         try:
-            if status.lang == "en" and status.user.screen_name not in users_tweeted: 
-                keywords = Rake.run(status.text.replace('#', ''))
-                book = amazonBooker.get_book_by_keywords(' or '.join([x[0] for x in keywords[:2]]))
+            if status.lang == "en":
+                keywords = Rake.run(status.text.replace('#', '').replace('@nextbookbot', ''))
+                books = amazonBooker.get_similar_books(' '.join([x[0] for x in keywords[:4]]), 4)
+                book = random.choice(books)
 
-                time.sleep(random.randrange(60,120))
                 # Tweet to the User who Tweeted it 
-                status_txt = random.choice(tweet_body).format(status.user.screen_name, book.title[:20], book.offer_url)
-
-                users_tweeted.append(status.user.screen_name)
+                status_txt = random.choice(tweet_body).format(status.user.screen_name, book.title[:30], book.offer_url)
                 tweeter.update_status(status_txt, status.id)
 
                 print(status_txt)
@@ -46,9 +37,9 @@ if __name__ == "__main__":
                     log.write('\n')
 
         except amazon.api.SearchException:
-            pass
+            print("Hey @{} I can't find that Book, it must be me ,can you try once more with the exact name of the book?".format(status.user.screen_name))
 
 
     tweeter.create_stream(process_status)
-    tweeter.track_stream(['#suggestions', '#books'])
+    tweeter.track_stream(['@nextbookbot', "#mynextread"])
 
